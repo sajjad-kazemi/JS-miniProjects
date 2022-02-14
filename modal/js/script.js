cd3f77367e8968fe9d370dbfbfaad856db06694f
Sajjad-Kazemi <sajjad9410kazemi@gmail.com>
2022-02-14 18:58:39 +0330

    modal project

---
+++ modal/js/script.js
@@ -0,0 +1,27 @@
+var $ = document
+const modalParent =  $.getElementById("modal-parent")
+const xBtn = $.getElementById("X")
+const showModal = $.getElementById("show-modal")
+const section = $.querySelector("section")
+
+xBtn.addEventListener("click",closeModalWithX)
+showModal.addEventListener("click",openModal)
+modalParent.addEventListener("click",closeModal)
+
+
+function closeModalWithX(e) {
+    modalParent.style.display = "none"
+    section.style.filter = ""
+}
+function openModal(e) {
+    const target = $.getElementById(e.target.dataset.target)
+    section.style.filter = "blur(8px)"
+    target.style.display = 'inline'
+}
+function closeModal(e){
+    console.log(e.target)
+    if(e.target == modalParent){
+        modalParent.style.display = "none"
+        section.style.filter = ""
+    }
+}
